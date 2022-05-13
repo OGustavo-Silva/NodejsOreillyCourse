484e821c181b99ad8490bf371e0c19311700781f
Gustavo <gustavos_barros@hotmail.com>
2022-05-13 11:59:33 -0300

    add note to file deployed

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -3,8 +3,33 @@ const getNotes = function(){
     return 'Your notes...'
 }
 
-const addNote = function(){
+const addNote = function(title, body){
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function(note){
+        return note.title === title
+    })
 
+    if(duplicateNotes.length === 0){
+        notes.push({
+            title: title,
+            body: body
+        })
+    
+        saveNotes(notes)
+        console.log('New note added!')
+    }else{
+        console.log('Note title taken!')
+    }
+
+}
+
+const removeNote = function(title){
+    const notes = loadNotes()
+}
+
+const saveNotes = function(notes){
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
 }
 
 const loadNotes = function(){
@@ -13,7 +38,7 @@ const loadNotes = function(){
         const dataJSON = dataBuffer.toString()
         return JSON.parse(dataJSON)
     }catch(e){
-
+        return []
     }
     
 }
