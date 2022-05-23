f35a3d495dd70325603d2fd59c24e37227ae8460
Gustavo <gustavos_barros@hotmail.com>
2022-05-23 17:18:56 -0300

    starting with http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('request') //npm i request@2.88.0
 
-setTimeout(() => {
-    console.log('2 Second Timer')
-}, 2000)
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=32&lon=46&appid=bf00f9c8ad3a0e102fe5d2d0710178e1'
 
-setTimeout(() => {
-    console.log('0 Second Timer')
-}, 0);
-
-console.log('Stopping')
+request({ url:url }, (erorr, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data)
+})
