c7340258df2e9a012d62c165945997d4fa5a5f86
Gustavo da Silva Barros <gustavos_barros@hotmail.com>
2022-05-29 18:49:59 -0300

    customizing http request

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,9 @@
 const request = require('request') //npm i request@2.88.0
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=32&lon=46&appid=bf00f9c8ad3a0e102fe5d2d0710178e1'
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=-23.618&lon=-46.635&appid=bf00f9c8ad3a0e102fe5d2d0710178e1&units=metric'
 
-request({ url:url }, (erorr, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data)
+request({ url:url, json:true }, (erorr, response) => {
+    console.log(response.body.weather[0].main + '\nIt\'s currently ' +
+        response.body.main.temp + 'Cº' +
+        '\nAnd it feels like ' + response.body.main.feels_like + 'Cº')
 })
