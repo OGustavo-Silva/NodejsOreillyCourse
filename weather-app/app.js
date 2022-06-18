850f5e1bf7c513d2579618aa79dbe041fb35e8e3
Gustavo da Silva Barros <gustavos_barros@hotmail.com>
2022-06-18 13:49:14 -0300

    accepting city name on command terminal

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,13 +1,22 @@
-const request = require('request') //npm i request@2.88.0
 const geocode = require('./utils/geocode.js')
 const forecast = require('./utils/forecast.js')
 
-// geocode('Boston', (error, data) =>{
-//     console.log('Error', error)
-//     console.log('Data', data)
-// })
-
-forecast(-75.7088, 44.1545, (error, data) => {
-    console.log('Error', error)
-    console.log('Data', data)
-})
+const address = process.argv[2]
+if(!address){
+    console.log('Please provide an address!')
+}else{
+    geocode(address, (error, data) =>{
+        if(error){
+            return console.log(error)
+        }
+    
+        forecast(data.latitude, data.longitude, (error, forecastData) => {
+            if(error){
+                return console.log(error)
+            }
+    
+            console.log(data.location)
+            console.log(forecastData)
+        })
+    })
+}
