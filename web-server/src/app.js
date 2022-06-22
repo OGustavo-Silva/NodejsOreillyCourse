f72018fdf51cbd18c1f19a7142e8172921890ee1
Gustavo <gustavos_barros@hotmail.com>
2022-06-22 16:21:35 -0300

    serving dynamic content with handlebars

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -4,7 +4,27 @@ const express = require('express')
 const app = express()
 const publicDirectoryPath = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')//setup handlebars | npm i hbs@4.0.1
 app.use(express.static(publicDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Gustavo'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About page'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('about', {
+        helpText: 'Help page'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send({
